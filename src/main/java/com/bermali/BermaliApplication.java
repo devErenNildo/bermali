432a3c5f60a2f544a2f1b38c7623cd13b94f2a48
devEren <dev.erenildo@gmail.com>
2024-09-09 15:42:45 -0400

    create database docker

---
+++ src/main/java/com/bermali/BermaliApplication.java
@@ -0,0 +1,13 @@
+package com.bermali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BermaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BermaliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bermali/BermaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bermali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BermaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
