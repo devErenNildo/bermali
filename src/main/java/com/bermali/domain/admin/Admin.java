e0c77eaf3fd0c5d4a93d9cc12b4d6004c6e6b699
devEren <dev.erenildo@gmail.com>
2024-09-15 15:19:28 -0400

    create class domain Admin

---
+++ src/main/java/com/bermali/domain/admin/Admin.java
@@ -0,0 +1,47 @@
+package com.bermali.domain.admin;
+
+import java.util.UUID;
+
+public class Admin {
+    private UUID id;
+    private String name;
+    private String email;
+    private String password;
+
+    // CONSTRUCTOR  --------------------------------------------------------
+
+    public Admin(){
+    }
+
+    public Admin(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    // GETTERS AND SETTERS -------------------------------------------------
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
