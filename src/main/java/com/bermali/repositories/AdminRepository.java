80e6f4cfd0c3614baa58a8c1ef8f67ffa9f16bc9
devEren <dev.erenildo@gmail.com>
2024-09-16 16:27:11 -0400

    create repository admin

---
+++ src/main/java/com/bermali/repositories/AdminRepository.java
@@ -0,0 +1,9 @@
+package com.bermali.repositories;
+
+import com.bermali.domain.admin.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AdminRepository extends JpaRepository<Admin, UUID> {
+}
