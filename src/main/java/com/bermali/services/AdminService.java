f0b47f5fe30cc6051a9f428929c1dfe51e193237
devEren <dev.erenildo@gmail.com>
2024-09-20 13:22:29 -0400

    encoding password

--- src/main/java/com/bermali/services/AdminService.java
+++ src/main/java/com/bermali/services/AdminService.java
@@ -1,6 +1,7 @@
 package com.bermali.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.bermali.domain.admin.Admin;
@@ -11,8 +12,14 @@ public class AdminService {
 	
 	@Autowired
 	private AdminRepository adminRepository;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	public Admin createAdmin(Admin admin) {
+
+		var password = passwordEncoder.encode(admin.getPassword());
+		admin.setPassword(password);
 		return adminRepository.save(admin);
 	}
 	
